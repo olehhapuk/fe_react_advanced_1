33510bba3929d72cd05521a68b6691486ac6705b
olehhapuk <oleggapuk20@gmail.com>
2021-09-19 10:46:04 +0300

    Moved to es modules

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const app = require('./src/app');
+import app from './src/app.js';
 
 const port = process.env.PORT;
 app.listen(port, () => {
--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
-const express = require('express');
-const volleyball = require('volleyball');
-const path = require('path');
-require('dotenv').config();
-// import dotenv from 'dotenv';
-// dotenv.config();
+import express from 'express';
+import volleyball from 'volleyball';
+import path from 'path';
+import dotenv from 'dotenv';
 
-const articles = require('./routes/articles');
+import { a } from './routes/index.js';
+
+console.log(a);
+
+dotenv.config();
 
 const app = express();
 
@@ -15,9 +17,4 @@ app.set('view engine', 'ejs');
 app.use(volleyball);
 app.use(express.static(path.join(__dirname + '../public')));
 
-// GET /articles
-// GET /articles/:id
-// POST /articles
-app.use('/articles', articles);
-
-module.exports = app;
+export default app;
