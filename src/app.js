4f8565a813ca5562f6d4d91458490578846ee918
olehhapuk <oleggapuk20@gmail.com>
2021-09-19 12:05:18 +0300

    Cookies and sessions

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const volleyball = require('volleyball');
 const path = require('path');
+const cookieParser = require('cookie-parser');
+const expressSession = require('express-session');
 require('dotenv').config();
 // import dotenv from 'dotenv';
 // dotenv.config();
@@ -14,10 +16,41 @@ app.set('view engine', 'ejs');
 
 app.use(volleyball);
 app.use(express.static(path.join(__dirname + '../public')));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  expressSession({
+    resave: false,
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: false,
+  })
+);
 
 // GET /articles
 // GET /articles/:id
 // POST /articles
 app.use('/articles', articles);
+
+app.get('/cookie', (req, res) => {
+  console.log(req.cookies.message);
+  console.log(req.signedCookies.secret);
+  res.render('cookie');
+});
+
+app.get('/set-cookie', (req, res) => {
+  res.cookie('message', 'Hello, World!');
+  res.cookie('secret', 'hi', { signed: true });
+  res.redirect('/cookie');
+});
+
+app.get('/clear-cookie', (req, res) => {
+  res.clearCookie('message');
+  res.clearCookie('secret');
+  res.redirect('/cookie');
+});
+
+app.get('/views', (req, res) => {
+  req.session.views = req.session.views ? req.session.views + 1 : 1;
+  res.render('views', { views: req.session.views });
+});
 
 module.exports = app;
