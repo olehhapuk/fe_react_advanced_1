13692c4a9da0930d9fb42d0f2d455402f27703d5
olehhapuk <oleggapuk20@gmail.com>
2021-09-19 13:11:53 +0300

    GET, POST /users

--- src/app.js
+++ src/app.js
@@ -3,9 +3,14 @@ const volleyball = require('volleyball');
 const path = require('path');
 require('dotenv').config();
 
+const users = require('./routes/users');
+
 const app = express();
 
 app.use(volleyball);
 app.use(express.static(path.join(__dirname + '../public')));
+app.use(express.json());
+
+app.use('/api/users', users);
 
 module.exports = app;
