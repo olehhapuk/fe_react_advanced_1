5870890c1ad148f1bc98a0faca7bca51e37cc17e
olehhapuk <oleggapuk20@gmail.com>
2022-05-01 17:36:13 +0300

    Websocket server and client connection

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-const app = require('./src/app');
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+const http = require('http');
+const { Server } = require('socket.io');
 
-app.listen(process.env.PORT, () => {
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static('./public'));
+
+let users = [];
+
+io.on('connection', (socket) => {
+  users.push(socket.id);
+
+  io.emit('user connected', { userId: socket.id, users });
+
+  socket.on('disconnect', () => {
+    users = users.filter((userId) => socket.id !== userId);
+    io.emit('user disconnected', { userId: socket.id, users });
+  });
+});
+
+server.listen(process.env.PORT, () => {
   console.log(`Started on http://localhost:${process.env.PORT}`);
 });
--- src/middlewares/isEmailVerified.js
+++
@@ -1,4 +0,0 @@
-module.exports = (req, res, next) =>
-  req.user.isVerified
-    ? next()
-    : res.status(401).send('Спочатку підтвердіть E-Mail');
