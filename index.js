95af766d0403b23e14253d6b4d1eeb2b0eba1fb1
olehhapuk <oleggapuk20@gmail.com>
2022-05-15 12:55:06 +0300

    Created basic prisma setup

--- index.js
+++ index.js
@@ -1,84 +1,30 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
-const http = require('http');
-const { Server } = require('socket.io');
-const { nanoid } = require('nanoid');
+const { PrismaClient } = require('@prisma/client');
 
-const createMessageHandlers = require('./handlers/messageHandlers');
-
+const prisma = new PrismaClient();
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static('./public'));
 
-// Repository
+app.post('/', async (req, res) => {
+  try {
+    const newUser = await prisma.user.create({
+      data: {
+        password: 'pass',
+        username: 'user',
+      },
+    });
 
-let users = [];
-let messages = [];
-
-function createUser(socket, username) {
-  return {
-    id: socket.id,
-    username,
-  };
-}
-
-function findUserById(userId) {
-  return users.find((user) => user.id === userId);
-}
-
-function createMessage(socket, text, targetUserId) {
-  return {
-    id: nanoid(),
-    text,
-    from: findUserById(socket.id),
-    // to: findUserById(targetUserId)
-  };
-}
-
-function insertUser(user) {
-  users.push(user);
-}
-
-function insertMessage(message) {
-  messages.push(message);
-}
-
-// ./Repository
-
-function init(io, socket) {
-  const newUser = createUser(socket, socket.handshake.auth.username);
-  insertUser(newUser);
-
-  socket.broadcast.emit('user connected', newUser);
-  io.to(socket.id).emit('users list', users);
-  io.to(socket.id).emit('messages list', messages);
-}
-
-io.on('connection', (socket) => {
-  socket.on('disconnect', () => {
-    users = users.filter((userId) => socket.id !== userId);
-    io.emit('user disconnected', { userId: socket.id, users });
-  });
-
-  init(io, socket);
-
-  const messageHandlers = createMessageHandlers(
-    io,
-    socket,
-    createMessage,
-    insertMessage,
-    findUserById
-  );
-
-  socket.on('message create', messageHandlers.handleMessageCreate);
-  socket.on('message typing', messageHandlers.handleMessageTyping);
+    res.json(newUser);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error.message);
+  }
 });
 
-server.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Started on http://localhost:${process.env.PORT}`);
 });
